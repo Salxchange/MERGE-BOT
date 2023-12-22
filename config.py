21773622cdfbe708a120f851056e765435aa5559
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-22 16:46:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config(object):
     DATABASE_URL = ("mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = ("-1001621207300")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    USER_SESSION_STRING =
+    USER_SESSION_STRING = ("BAHHO64AHHWBWQBH6vuhTTbrUBRzQX9oMonfyjflQV6tcGcPIJzvQTe21pa_85kWZteBsFzUwaMlVSMAabJ7mefSKf_tnyF6Rj9wkgzt9apibMpipIVxlzgU32G7YbK1-8xTep9w8ItMckqbxFihjN3R6B0mSEw7zk2vTXDR5QiNMGB-uyiehF5UTcQYI8vFFNjSPSHebunljTj7W3Yj7kbX6rnQ7zuOEHk_e67717WA7FmhX94mnAORG-0bCEovYukIQb6IgPj-9bZRnDQvm4hAab4c0Tmd_P7UgZy81jaVacsw1zk9FLpTUKhTFpPgU0w2uRL-C1p2ikHe-kjkPOTuLfEPggAAAAEv3sphAA")
     IS_PREMIUM = False
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
