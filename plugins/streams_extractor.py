42e8c7c8edbb8e6c90d1c607813a53185f1c8c1a
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-22 19:21:18 +0100

    Update streams_extractor.py

--- plugins/streams_extractor.py
+++ plugins/streams_extractor.py
@@ -9,10 +9,10 @@ from __init__ import LOGGER, gDict, queueDB
 import os
 from bot import delete_all
 from helpers.display_progress import Progress
-from helpers.ffmpeg_helper import extractAudios, extractSubtitles,extractMediaInfo
+from helpers.ffmpeg_helper import extractAudios, extractSubtitles,extractVideos
 from helpers.uploader import uploadFiles
 
-async def streamsExtractor(c: Client, cb:CallbackQuery ,media_mid, exAudios=False, exSubs=False, extractMediaInfo=False):
+async def streamsExtractor(c: Client, cb:CallbackQuery ,media_mid, exAudios=False, exSubs=False, extractVideos=False):
     if not os.path.exists(f"downloads/{str(cb.from_user.id)}/"):
         os.makedirs(f"downloads/{str(cb.from_user.id)}/")
     _hold = await cb.message.edit(text="Please wait")
@@ -55,9 +55,9 @@ async def streamsExtractor(c: Client, cb:CallbackQuery ,media_mid, exAudios=Fals
     if exSubs:
         await _hold.edit_text("Extracting Subtitles")
         extract_dir = await extractSubtitles(file_dl_path, cb.from_user.id)
-      if exMediaInfo:
-        await _hold.edit_text("Extracting Mediainfo")
-        extract_dir = await extractMediaInfo(file_dl_path, cb.from_user.id)
+      if exVideos:
+        await _hold.edit_text("Extracting Video")
+        extract_dir = await extractVideos(file_dl_path, cb.from_user.id)
 
     if extract_dir is None:
         await cb.message.edit("❌ Failed to Extract Streams !")
