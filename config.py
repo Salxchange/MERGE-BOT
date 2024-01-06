df3d039d6ff3dfa0321c3cf805957dcc1c120bc9
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-01-06 17:25:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     BOT_TOKEN = ("6408919661:AAGorbLd42BMvN93nUL1ehidDkgukGo5XAY")
     TELEGRAM_API = ("21027612")
     OWNER = ("1966867320")
-    OWNER_USERNAME = ("Salazar5000")
+    OWNER_USERNAME = ("None")
     PASSWORD = ("iam_roku")
     DATABASE_URL = ("mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = ("-1001560632964")  # Add channel id as -100 + Actual ID
