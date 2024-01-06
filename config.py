8caed466e4fd9ebc4b37f4f1659f95c9aff14c9a
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-01-06 17:05:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 class Config(object):
     API_HASH =("b36c5dc986f77eedd4bbf356b65eab19")
-    BOT_TOKEN = ("6702684341:AAFqjTr8dw4UJsie1Kk_-UqC0Ao87UaEKJs")
+    BOT_TOKEN = ("6408919661:AAGorbLd42BMvN93nUL1ehidDkgukGo5XAY")
     TELEGRAM_API = ("21027612")
-    OWNER = ("5098097249")
+    OWNER = ("1966867320")
     OWNER_USERNAME = ("Salazar5000")
     PASSWORD = ("iam_roku")
     DATABASE_URL = ("mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
