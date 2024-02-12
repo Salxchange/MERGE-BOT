6bea9682dbf091d458f88dd13acfbbec36198bde
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-02-12 01:40:47 +0100

    Update usettings.py

--- plugins/usettings.py
+++ plugins/usettings.py
@@ -44,9 +44,9 @@ async def userSettings(
 <b><u>Merge Bot settings for <a href='tg://user?id={uid}'>{fname} {lname}</a></u></b>
     ╭──────────────────────
     ┣**🎭 ID: <u>{usettings.user_id}</u>**
-    ┣**{'🪺' if usettings.banned else} Ban Status: <u>{usettings.banned}</u>**
-    ┣**{'🌦️' if usettings.allowed else} Allowed: <u>{usettings.allowed}</u>**
-    ┣**{'🏜️' if usettings.edit_metadata else} Edit Metadata: <u>{usettings.edit_metadata}</u>**
+    ┣**{'❌' if usettings.banned else '🪺'} Ban Status: <u>{usettings.banned}</u>**
+    ┣**{'🌦️' if usettings.allowed else '🌦️'} Allowed: <u>{usettings.allowed}</u>**
+    ┣**{'🏜️' if usettings.edit_metadata else '❌'} Edit Metadata: <u>{usettings.edit_metadata}</u>**
     ┣**🚏 Merge mode: <u>{userMergeModeStr}</u>**
     ╰──────────────────────
 """
