6d2ee933a20c0a343fc66fb0cad6e10c71ee487b
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-10 19:05:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     DATABASE_URL = ("mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = ("-1001560632964")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = True
     START_PIC = ("https://telegra.ph/file/bb0690bfacfdc008ff788.jpg")
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
